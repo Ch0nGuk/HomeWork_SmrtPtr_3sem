9e7e28ccd5e7451ebe109c0bb1fb12c0d5289fa4
Ch0nGuk <matvey@sorokopudov.ru>
2026-09-16 14:06:27 +0300

    add Storage.cpp

---
+++ Storage.cpp
@@ -0,0 +1,22 @@
+#include "Storage.h"
+
+
+namespace smrt::detail
+{
+    Storage::Storage(void* object, void (*deleter)(void*)) : object_(object), ref_count_(1), deleter_(deleter) {}
+    void Storage::IncCount() noexcept
+    {
+        ref_count_++;
+    }
+
+    void Storage::DecCount() noexcept
+    {
+        ref_count_--;
+        if (ref_count_ == 0)
+        {
+            deleter_(object_);
+            delete this;
+        }
+    }
+
+} // namespace smrt::detail
--- Storage.h
+++ Storage.h
@@ -2,25 +2,33 @@
 
 #include <cstddef>
 
+
+namespace smrt
+{
+    template <class T> class SmrtPtr;
+}
+
 namespace smrt::detail
 {
-
 class Storage
 {
-template <class T> class SmrtPtr friend;    
+template <class T> friend class smrt::SmrtPtr;
 public:
+    Storage(void* object, void (*deleter)(void*)); 
+    ~Storage() = default;
+
     Storage(const Storage&) = delete;
     Storage(Storage&&) = delete;
     Storage& operator=(const Storage&) = delete;
     Storage& operator=(Storage&&) = delete;
+
 private:
     void* object_;
     size_t ref_count_;
-    void (*deleter)(void*); 
-    Storage(void* object, void (*deleter)(void*)); 
+    void (*deleter_)(void*); 
 
-    void DecCount();
-    void IncCount();
+    void DecCount() noexcept;
+    void IncCount() noexcept;
 };
 
 template <class T>
@@ -28,6 +36,4 @@ void delete_object(void* object)
 {
     delete static_cast<T*>(object); // приводим объект к указателю на тип T и удаляем
 }
-
-
-} 
+} // namespace smrt::detil
